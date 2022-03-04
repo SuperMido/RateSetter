581cec90aba9c5c32b069fc3248671e691f45e12
Huy Tran <quanghuy.tran@nfq.asia>
2022-03-04 10:55:47 +0700

    Add Address model

---
+++ Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RateSetter.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public string StreetAddress { get; set; }
+        public string Suburb { get; set; }
+        public string State { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
