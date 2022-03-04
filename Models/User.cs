7d165717997dc5861ea534db73f04d3098c2f05e
Huy Tran <quanghuy.tran@nfq.asia>
2022-03-04 10:59:30 +0700

    Create User model

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RateSetter.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string ReferralCode { get; set; }
+        [Display(Name = "Address")]
+        public int AddressId { get; set; }
+        [ForeignKey("AddressId")]
+        public virtual Address Address { get; set; }
+    }
+}
